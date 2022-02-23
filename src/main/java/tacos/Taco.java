6ff096874e43849e9934038806b2f837462183a5
Fuad <kamilovfuad@gmail.com>
2022-02-23 20:53:35 +0400

    Fix the project to fit Spring Data Jpa requirements for usage

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -3,18 +3,22 @@ package tacos;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
 import tacos.IngredientRef;
 
 @Data
-@Table
+@Entity
 public class Taco {
     
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     
     private Date createdAt = new Date();
@@ -23,11 +27,11 @@ public class Taco {
     @Size(min=5, message="name must be at least 5 characters long")
     private String name;
     
-    @NotNull
     @Size(min=1, message="You must choose at least 1 ingredient")
-    private List<IngredientRef> ingredients = new ArrayList<>();
+    @ManyToMany()
+    private List<Ingredient> ingredients = new ArrayList<>();
     
-    public void addIngredient(Ingredient taco) {
-        this.ingredients.add(new IngredientRef(taco.getId()));
+    public void addIngredient(Ingredient ingredient) {
+        this.ingredients.add(ingredient);
     }
 }

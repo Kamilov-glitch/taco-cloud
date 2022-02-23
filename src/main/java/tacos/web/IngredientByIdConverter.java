8f8eb3a3c8e466429fe34381ab6781d65fa8aa4e
Fuad <kamilovfuad@gmail.com>
2022-02-23 20:15:56 +0400

    Add necessary Autowired annotation to IngredientByIdConverter

--- src/main/java/tacos/web/IngredientByIdConverter.java
+++ src/main/java/tacos/web/IngredientByIdConverter.java
@@ -2,6 +2,7 @@ package tacos.web;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 import tacos.Ingredient;
@@ -12,7 +13,8 @@ import tacos.data.IngredientRepository;
 public class IngredientByIdConverter implements Converter<String, Ingredient> {
 
     private IngredientRepository ingredientRepo;
-
+    
+    @Autowired
     public IngredientByIdConverter(IngredientRepository ingredientRepo) {
        this.ingredientRepo = ingredientRepo;
     }

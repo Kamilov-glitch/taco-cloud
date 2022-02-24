605bcefd060888298bcac939ed60c81b6b786d91
Fuad <kamilovfuad@gmail.com>
2022-02-24 14:40:20 +0400

    Add security dependency to pom

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -11,7 +11,6 @@ import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import lombok.Data;
-import tacos.IngredientRef;
 
 @Data
 @Entity
